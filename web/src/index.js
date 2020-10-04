8c2931023159ab4cdec39d143925296c5313e664
Phill Sparks <me@phills.me.uk>
2020-10-04 11:52:31 +0100

    Setup TailwindCSS

--- web/src/index.js
+++ web/src/index.js
@@ -1,4 +1,5 @@
-import { Elm } from "./src/Main.elm";
+import { Elm } from "./Main.elm";
+import "./assets/css/main.pcss";
 
 document.addEventListener("DOMContentLoaded", function () {
     const mountPoint = document.querySelector("main");
