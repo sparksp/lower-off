baa7c1bb5f2877888920476b47fe424e13ee7e67
Phill Sparks <me@phills.me.uk>
2020-10-21 21:54:53 +0100

    Add strip to clean language

--- web/src/Climb.elm
+++ web/src/Climb.elm
@@ -31,7 +31,7 @@ string : Climb -> String
 string climb =
     case climb of
         LeadAndClean ->
-            "You are the last person to lead this climb, please clean it when you've finished."
+            "You are the last person to lead this climb, please clean / strip it when you've finished."
 
         LeadAndLead ->
             "You are leading this climb and others want to lead it after you."
@@ -43,7 +43,7 @@ string climb =
             "You are leading this climb and others want to top-rope it after you."
 
         Second ->
-            "You are seconding this climb, please clean it when you've finished."
+            "You are seconding this climb, please clean / strip it when you've finished."
 
         TopRope ->
-            "You are the last person to top-rope this climb, please clean it when you've finished."
+            "You are the last person to top-rope this climb, please clean / strip it when you've finished."
